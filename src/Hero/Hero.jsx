1a799e11b8a58803ed7d21abf42c4a092dfb0c5b
Muzamil Hussain <hmuzamil567@gmail.com>
2024-02-04 22:11:57 +0500

    working on hero section styling

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -3,27 +3,25 @@ import "./Hero.css";
 
 const Hero = () => {
   return (
-    <>
-      <div className="absolute w-4/5 h-dvh bg-black opacity-80"></div>
-      <div className="w-4/5 h-dvh relative image">
-        <div className="w-4/5 h-fit absolute inset-1/2 -translate-x-1/2 -translate-y-1/2">
-          <div className="text-white text-center font-medium">
-            <h1 className="text-2xl">Welcome</h1>
-            <p className="text-6xl my-4">
-              I'm <span>Muzamil Hussain</span>
-            </p>
-            <span className="text-2xl text-gray-100">
-              based in Lahore, Pakistan
-            </span>
-          </div>
-          <div className="flex justify-center mt-6">
-            <button className="border-2 border-green text-green px-8 py-2 rounded-full">
-              Hire Me
-            </button>
-          </div>
+    <div className="w-4/5 h-dvh relative image">
+      <div className="absolute w-full h-dvh bg-black opacity-80"></div>
+      <div className="w-4/5 h-fit absolute inset-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="text-white text-center font-medium">
+          <h1 className="text-2xl">Welcome</h1>
+          <p className="text-6xl my-4">
+            I'm <span>Muzamil Hussain</span>
+          </p>
+          <span className="text-2xl text-gray-100">
+            based in Lahore, Pakistan
+          </span>
+        </div>
+        <div className="flex justify-center mt-10">
+          <button className="border-2 border-green text-green px-8 py-2 rounded-full hover:bg-green hover:text-white transition-all">
+            Hire Me
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
