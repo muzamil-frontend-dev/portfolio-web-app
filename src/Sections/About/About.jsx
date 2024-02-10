4d534002a46136c2f78636777a72dcae88f15354
Muzamil Hussain <hmuzamil567@gmail.com>
2024-02-10 16:26:33 +0500

    update numbers counter to run only once

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import CountUp from "react-countup";
-import VisibilitySensor from "react-visibility-sensor";
 import Heading from "../../Components/Heading/Heading";
 
 const About = () => {
@@ -57,52 +56,25 @@ const About = () => {
       <ul className="flex flex-row flex-wrap text-center mt-12">
         <li className="lg:border-r lg:border-slate-200 py-5 px-3 basis-1/2 lg:basis-1/4">
           <p className="text-2xl md:text-4xl lg:text-5xl font-medium leading-snug text-slate-300">
-            <CountUp end={10} duration={2}>
-              {({ countUpRef, start }) => (
-                <VisibilitySensor onChange={start} delayCall>
-                  <span ref={countUpRef} />
-                </VisibilitySensor>
-              )}
-            </CountUp>
-            +
+            <CountUp end={10} duration={2} enableScrollSpy scrollSpyOnce />+
           </p>
           <span>Years Experience</span>
         </li>
         <li className="lg:border-r lg:border-slate-200 py-5 px-3 basis-1/2 lg:basis-1/4">
           <p className="text-2xl md:text-4xl lg:text-5xl font-medium leading-snug text-slate-300">
-            <CountUp end={250} duration={2}>
-              {({ countUpRef, start }) => (
-                <VisibilitySensor onChange={start} delayCall>
-                  <span ref={countUpRef} />
-                </VisibilitySensor>
-              )}
-            </CountUp>
-            +
+            <CountUp end={250} duration={2} enableScrollSpy scrollSpyOnce />+
           </p>
           <span>Happy clients</span>
         </li>
         <li className="lg:border-r lg:border-slate-200 py-5 px-3 basis-1/2 lg:basis-1/4">
           <p className="text-2xl md:text-4xl lg:text-5xl font-medium leading-snug text-slate-300">
-            <CountUp end={650} duration={2}>
-              {({ countUpRef, start }) => (
-                <VisibilitySensor onChange={start} delayCall>
-                  <span ref={countUpRef} />
-                </VisibilitySensor>
-              )}
-            </CountUp>
-            +
+            <CountUp end={650} duration={2} enableScrollSpy scrollSpyOnce />+
           </p>
           <span>projects Done</span>
         </li>
         <li className="py-5 px-3 basis-1/2 lg:basis-1/4">
           <p className="text-2xl md:text-4xl lg:text-5xl font-medium leading-snug text-slate-300">
-            <CountUp end={38} duration={2}>
-              {({ countUpRef, start }) => (
-                <VisibilitySensor onChange={start} delayCall>
-                  <span ref={countUpRef} />
-                </VisibilitySensor>
-              )}
-            </CountUp>
+            <CountUp end={38} duration={2} enableScrollSpy scrollSpyOnce />
           </p>
           <span>Get Awards</span>
         </li>
